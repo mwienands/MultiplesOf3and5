7aec8205d008103e201160619b048e3d3b1368ed
mwienands <mwienands@gmail.com>
2017-09-22 13:16:09 -0500

    Working C++ code to solve the 3 and 5 divisibility problem

--- MultiplesOf3and5/main.cpp
+++ MultiplesOf3and5/main.cpp
@@ -5,11 +5,27 @@
 //  Created by Michael Wienands on 9/22/17.
 //  Copyright © 2017 Michael Wienands. All rights reserved.
 //
+//https://github.com/mwienands/MultiplesOf3and5.git
+//If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
+//The sum of these multiples is 23.
+//Find the sum of all the multiples of 3 or 5 below 1000.
+//
 
 #include <iostream>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    int total= 0;
+    
+    for(int i = 1;i<1000; i++ ){
+        if(i%3 == 0){
+            total += i;
+            
+        }else if(i%5 == 0){
+            total +=i;
+            
+        }
+    }
+    
+    std::cout << total;
     return 0;
 }
